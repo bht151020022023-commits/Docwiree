e2883d0b11bc2ff7e8db39dd24572a6dd9a37646
bht151020022023-commits <bht151020022023@gmail.com>
2026-06-22 09:29:24 +0700

    Create question.js

---
+++ question.js
@@ -0,0 +1,61 @@
+let questions = [
+{
+  question:"comprises/ extending from frame 1 to frame 24/ The lower section of the fuselage/ 3 skin panels.",
+  answers:[
+    "The lower section of the fuselage comprises 3 skin panels extending from frame 1 to frame 24",
+    "The lower fuselage of the section comprises 3 skin panels extending from frame 1 to frame 24",
+    "The lower section comprises 3 skin panels extending from frame 1 to frame 24 of the fuselage"
+  ],
+  correct:0
+},
+
+{
+  question:"Conical/ rounded/with/ end",
+  answers:[
+    "Conical with end rounded",
+    "End with rounded conical",
+    "Conical with rounded end"
+  ],
+  correct:2
+},
+
+{
+  question:"than / the / wiring / diagram / the / test / is / more / complicated",
+  answers:[
+    "The test is more than complicated the wiring diagram",
+    "The test is more complicated than the wiring diagram",
+    "The diagram test is more complicated than the wiring"
+  ],
+  correct:1
+},
+
+{
+  question:"(increase) engine thrust, (advance) the throttle levers.",
+  answers:[
+    "To increase engine thrust, advance the throttle levers.",
+    "To increasing engine thrust, advancing the throttle levers.",
+    "To increase engine thrust, advancing the throttle levers."
+  ],
+  correct:0
+},
+
+{
+  question:"With (increase) altitudes, the ambient pressure (decrease) and the temperature decreases continuously until 36000ft is reached.",
+  answers:[
+    "With increase altitudes, the ambient pressure decrease and the temperature decreases continuously until 36000ft is reached.",
+    "With increasing altitudes, the ambient pressure decreases and the temperature decreases continuously until 36000ft is reached.",
+    "With increases altitudes, the ambient pressure decreases and the temperature decreases continuously until 36000ft is reached."
+  ],
+  correct:1
+},
+
+{
+  question:"Most aircraft (have) two main gears and a (support) gear.",
+  answers:[
+    "Most aircraft have two main gears and a supporting gear.",
+    "Most aircraft has two main gears and a supporting gear.",
+    "Most aircraft have two main gears and a supported gear."
+  ],
+  correct:2
+},
+];
